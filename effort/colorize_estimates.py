4ddf1870ce01eb10c76c781a756a253b7866e3ce
Niko Föhr <fohrloop@gmail.com>
2024-10-19 17:53:53 +0300

    update colorize estimates: ability to give file and colors as input

--- effort/colorize_estimates.py
+++ effort/colorize_estimates.py
@@ -1,6 +1,6 @@
 from typing import Callable, Iterable
 
-from matplotlib.colors import LinearSegmentedColormap, Normalize, to_hex
+from matplotlib.colors import LinearSegmentedColormap, Normalize, to_hex, to_rgb
 
 
 def get_hex_func(
@@ -21,8 +21,20 @@ def get_hex_func(
 
 
 if __name__ == "__main__":
+    import sys
+
+    if len(sys.argv) > 1:
+        file = sys.argv[1]
+    else:
+        file = "effort-grid-estimates.txt"
+    try:
+        color_min = to_rgb(sys.argv[2])
+        color_max = to_rgb(sys.argv[3])
+    except IndexError:
+        color_min = (1, 1, 1)
+        color_max = (0.542, 0.211, 0.973)
     efforts = {}
-    with open("effort-grid-estimates.txt") as f:
+    with open(file) as f:
         for line in f:
             char, effort = line.split()
             effort = float(effort)
@@ -30,7 +42,9 @@ if __name__ == "__main__":
 
     mineffort = min(efforts.values())
     maxeffort = max(efforts.values())
-    get_color = get_hex_func(1.0, maxeffort / mineffort)
+    get_color = get_hex_func(
+        mineffort, maxeffort, min_color=color_min, max_color=color_max
+    )
 
     for char, effort in efforts.items():
         print(f"Char {char}: {effort:.2f}  color: {get_color(effort)}")
